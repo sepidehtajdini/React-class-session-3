c619c9f10666c11e4abeeebad297b666321e7164
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-05 14:02:48 +0330

    Update webpack.config.mjs

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -6,30 +6,27 @@ export default {
         path: path.resolve(".")
     },
 
-module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-        rules:[
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use:{
-                    loader: "babel-loader",
-                    options:{
-                        presets:[
-                            ["@babel/preset-env",{targets:{
-                                ie:"1",
-                                chrome:"50"
-                            }}]
-                        ]
-                    }
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        ["@babel/preset-env", {
+                            targets: {
+                                ie: "1",
+                                chrome: "50"
+                            }
+                        }]
+                    ]
                 }
             }
-        ],
-    },
-    
+        }]
+    }
 }

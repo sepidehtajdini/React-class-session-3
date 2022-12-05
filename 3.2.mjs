83e8626ab77cc94cb60df064ada22eebbdd6ae41
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-05 14:14:28 +0330

    Update 3.2.mjs

--- 3.2.mjs
+++ 3.2.mjs
@@ -1,35 +1,66 @@
 import fs from "fs";
 const address = "./loremipsum.txt";
 const file = fs.readFileSync(address, "utf-8");
-const newFile = file.replace(/[a-z]/g, function(str) {
-    let replacing = {
-        'a': 'z',
-        'b': 'y',
-        'c': 'x',
-        'd': 'w',
-        'e': 'v',
-        'f': 'u',
-        'g': 't',
-        'h': 's',
-        'i': 'r',
-        'j': 'q',
-        'k': 'p',
-        'l': 'o',
-        'm': 'n',
-        'n': 'm',
-        'o': 'l',
-        'p': 'k',
-        'q': 'j',
-        'r': 'i',
-        's': 'h',
-        't': 'g',
-        'u': 'f',
-        'v': 'e',
-        'w': 'd',
-        'x': 'c',
-        'y': 'b',
-        'z': 'a'
-    };
-    return replacing[str];
+const smallReplacing = {
+    'a': 'z',
+    'b': 'y',
+    'c': 'x',
+    'd': 'w',
+    'e': 'v',
+    'f': 'u',
+    'g': 't',
+    'h': 's',
+    'i': 'r',
+    'j': 'q',
+    'k': 'p',
+    'l': 'o',
+    'm': 'n',
+    'n': 'm',
+    'o': 'l',
+    'p': 'k',
+    'q': 'j',
+    'r': 'i',
+    's': 'h',
+    't': 'g',
+    'u': 'f',
+    'v': 'e',
+    'w': 'd',
+    'x': 'c',
+    'y': 'b',
+    'z': 'a'
+};
+const capitalReplacing = {
+    'A': 'Z',
+    'B': 'Y',
+    'C': 'X',
+    'D': 'W',
+    'E': 'V',
+    'F': 'U',
+    'G': 'T',
+    'H': 'S',
+    'I': 'R',
+    'J': 'Q',
+    'K': 'P',
+    'L': 'O',
+    'M': 'N',
+    'N': 'M',
+    'O': 'L',
+    'P': 'K',
+    'Q': 'J',
+    'R': 'I',
+    'S': 'H',
+    'T': 'G',
+    'U': 'F',
+    'V': 'E',
+    'W': 'D',
+    'X': 'C',
+    'Y': 'B',
+    'Z': 'A'
+};
+const newFile = file.replace(/[a-z]/g, function (str) {
+    return smallReplacing[str];
 });
-console.log(newFile)
+const finalFile = newFile.replace(/[A-Z]/g, function (str) {
+    return capitalReplacing[str];
+});
+console.log(finalFile);
